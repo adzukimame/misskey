5ae87d0dd0bd0417d526903bd2ec5d32c0618060
adzukimame <88547072+adzukimame@users.noreply.github.com>
2025-11-06 10:46:09 +0900

    Dockerのhealthcheckスクリプトを修正 (#374)

--- healthcheck.sh
+++ healthcheck.sh
@@ -3,5 +3,8 @@
 # SPDX-FileCopyrightText: syuilo and misskey-project
 # SPDX-License-Identifier: AGPL-3.0-only
 
-PORT=$(grep '^port:' /misskey/.config/default.yml | awk 'NR==1{print $2; exit}')
+if [ -z "$PORT" ]; then
+	PORT=$(grep '^port:' /misskey/.config/default.yml | awk 'NR==1{print $2; exit}')
+fi
+
 curl -Sfso/dev/null "http://localhost:${PORT}/healthz"
