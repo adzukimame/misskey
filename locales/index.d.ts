ec0829165a9413adfc6ecd1b6b09f8639f9a5f6b
adzukimame <88547072+adzuki02@users.noreply.github.com>
2025-11-25 23:37:35 +0900

    Cloudflare Challengesの期限切れでAPIコールが失敗した場合にTurnstileのウィジェットを表示してPre-clearanceを行うように

--- locales/index.d.ts
+++ locales/index.d.ts
@@ -10433,6 +10433,20 @@ export interface Locale extends ILocale {
          */
         "native": string;
     };
+    "_cloudflareChallenges": {
+        /**
+         * セキュリティチェック
+         */
+        "verificationRequired": string;
+        /**
+         * 続行するにはセキュリティチェックを完了してください。
+         */
+        "verificationDescription": string;
+        /**
+         * セキュリティチェックが完了しました。もう一度お試しください。
+         */
+        "verificationCompleted": string;
+    };
 }
 declare const locales: {
     [lang: string]: Locale;
