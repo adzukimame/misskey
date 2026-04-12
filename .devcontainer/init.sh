a26e17ef288cda58e5f2d642be321024dd16fc9b
adzukimame <88547072+adzukimame@users.noreply.github.com>
2026-04-12 18:31:23 +0900

    Dev Containerが動かないのを修正 (#395)

--- .devcontainer/init.sh
+++ .devcontainer/init.sh
@@ -6,7 +6,7 @@ sudo chown node node_modules
 sudo apt-get update
 sudo apt-get -y install libgtk2.0-0 libgtk-3-0 libgbm-dev libnotify-dev libnss3 libxss1 libasound2 libxtst6 xauth xvfb
 git config --global --add safe.directory /workspace
-git submodule update --init
+npm install -g corepack@latest
 corepack install
 corepack enable
 pnpm config set store-dir /home/node/.local/share/pnpm/store
