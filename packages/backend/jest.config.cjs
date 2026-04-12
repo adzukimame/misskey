786e0e4de33e3f07e93219806427d19fa144f2ce
adzukimame <88547072+adzukimame@users.noreply.github.com>
2026-04-12 21:53:29 +0900

    jest実行時に--no-experimental-require-moduleを追加

--- packages/backend/jest.config.cjs
+++ packages/backend/jest.config.cjs
@@ -181,7 +181,7 @@ module.exports = {
 
 	// A map from regular expressions to paths to transformers
 	transform: {
-		'^.+\\.(t|j)sx?$': ['@swc/jest'],
+		'^.+\\.tsx?$': ['@swc/jest'],
 	},
 
 	// An array of regexp pattern strings that are matched against all source file paths, matched files will skip transformation
